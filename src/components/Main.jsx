230066640aecddd919391416951b86faf12db23b
Gohar <aria.g@mail.ru>
2017-03-23 13:55:30 +0400

    search

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,23 +3,22 @@ import Todo from './ToDo.jsx';
 import WorkList from './WorkList.jsx';
 
 export default class Main extends Component {
-  constructor() {
-      super();
-      this.state = {
-          todos: [
-              {
-                  name: 'Work',
-                  work:[{wname:['coding', 'coffffeeeeeee', 'whatever']}]
-              }, {
-                  name: 'Private',
-                  work: [{wname: ['buy coffee', 'make coffee', 'drink coffee', 'shoooooppppping']}]
-              }
-          ],
-          selectedIndex: 0,
-          checked: true
-      };
-  }
-
+    constructor() {
+        super();
+        this.state = {
+            todos: [
+                {
+                    name: 'Work',
+                    work: ['coding', 'coffffeeeeeee', 'whatever']
+                }, {
+                    name: 'Private',
+                    work: ['buy coffee', 'make coffee', 'drink coffee', 'shoooooppppping']
+                }
+            ],
+            selectedIndex: 0,
+            checked: true
+        };
+    }
 
     onClickk = () => {
         console.log(this.state.checked)
@@ -43,7 +42,7 @@ export default class Main extends Component {
 
     onWorkAdd = (newWork) => {
         const {todos} = this.state;
-        todos[this.state.selectedIndex].work[0].wname.push(newWork);
+        todos[this.state.selectedIndex].work.push(newWork);
         this.setState({todos});
     };
 
@@ -51,8 +50,7 @@ export default class Main extends Component {
         return (
             <div>
                 <Todo todos={this.state.todos} onTodoRowClick={this.onRowSelect} onRowAdd={this.onRowAdd} onClickk={this.onClickk}/>
-                <WorkList works={this.state.todos[this.state.selectedIndex].work[0].wname} onClick={this.onClick} onClickk={this.onClickk}
-                  onWorkAdd={this.onWorkAdd}/>
+                <WorkList works={this.state.todos[this.state.selectedIndex].work} onClick={this.onClick} onClickk={this.onClickk} onWorkAdd={this.onWorkAdd}/>
             </div>
         );
     }
--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,13 +1,31 @@
 import React, {Component} from 'react';
 
 export default class Todo extends Component {
+  constructor() {
+      super();
+      this.state = {
+          search: ''
+      };
+  }
     addWork = (e) => {
         e.preventDefault();
-        this.props.onRowAdd({name: this.inputRef.value, work: {wname:['fooo']} });
+        this.props.onRowAdd({
+            name: this.inputRef.value,
+            work: {
+                wname: ['fooo'],
+                checked: true
+            }
+        });
         this.inputRef.value = "";
     }
 
     render() {
+      let todos = this.props.todos;
+      const search = this.state.search.toLowerCase();
+      todos = todos.filter(todos => todos.name.toLowerCase().match(search));
+      const rendering = todos.map((todos, i) => {
+          return <li key={i}>{todos.name}</li>
+      })
         return (
             <div>
                 <div>
@@ -26,10 +44,13 @@ export default class Todo extends Component {
                                         Worklist:{item.work[0].wname}</div>
                                 </li>
                             );
-
                         })}
                     </ul>
                 </div>
+                <input type="text" value={this.state.search} onChange={({target}) => this.setState({search: target.value})} placeholder="Filter..."/>
+                <ul>
+                    {rendering}
+                </ul>
             </div>
         )
     }
